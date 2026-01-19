9ba53dbd239c84e7c08086c82380671791b07833
Christine Izerek <daily.win.ai@gmail.com>
2026-01-19 14:23:11 -0600

    Stop tracking env.json and run_app.sh; ignore local secrets

--- run_app.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ~/updatewd
-
-flutter run -d chrome \
-  --dart-define=SUPABASE_URL=https://vmjcsflnqoqqvlmqnnyk.supabase.co \
-  --dart-define=SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtamNzZmxucW9xcXZsbXFubnlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1NjUzNDcsImV4cCI6MjA3NDE0MTM0N30.n5kHPhFjIBXic3pIHSk7sVa1xpzHfEYH5kpgTqMNu58
